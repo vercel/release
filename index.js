b8c3712e7e4d72862cc95e41e02038a839c763fe
Leo Lamprecht <mindrun@icloud.com>
2016-12-22 17:56:20 +0100

    Parse package.json file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// Native
+const path = require('path')
+
 // Packages
 const args = require('args')
 const {red} = require('chalk')
@@ -11,4 +14,15 @@ const abort = msg => {
   process.exit(1)
 }
 
-abort('LOL')
+const pkgPath = path.join(process.cwd(), 'package.json')
+let pkg
+
+try {
+  pkg = require(pkgPath)
+} catch (err) {
+  abort('Could not find a package.json file.')
+}
+
+
+console.log(pkg)
+abort('Ddd')
