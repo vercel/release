0ad974456216328c692bcd905bfc51a2e70c9c6a
Leo Lamprecht <mindrun@icloud.com>
2016-12-22 15:41:30 +0100

    Throw an error if no release type

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('test')
+#!/usr/bin/env node
+
+// Packages
+const args = require('args')
+const {red} = require('chalk')
+
+args.parse(process.argv)
+const releaseType = args.sub[0]
+
+const abort = msg => {
+  console.error(`${red('Error!')} ${msg}`)
+  process.exit(1)
+}
+
+if (!releaseType) {
+  abort('Please define a release type (major, minor or patch)!')
+}
+
+console.log(releaseType)
