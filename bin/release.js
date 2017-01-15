c87aa9a61f5a314f50fa6cc8b15a06a45944c59f
Leo Lamprecht <mindrun@icloud.com>
2017-01-15 14:46:27 +0100

    Moved checking for update to main binary

--- bin/release.js
+++ bin/release.js
@@ -2,11 +2,21 @@
 
 // Packages
 const asyncToGen = require('async-to-gen/register')
+const updateNotifier = require('update-notifier')
+
+// Ours
+const pkg = require('../package')
 
 // Support for keywords "async" and "await"
 asyncToGen({
   excludes: null
 })
+
+// Let user know if there's an update
+// This isn't important when deployed to Now
+if (!process.env.NOW) {
+  updateNotifier({pkg}).notify()
+}
 
 // Load package core with async/await support
 require('../lib')
