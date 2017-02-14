27663a6bcaa2cbd3ed69a82104ecbb91cfd34014
Leo Lamprecht <mindrun@icloud.com>
2017-02-14 21:43:02 +0100

    Use `bluebird` instead of async/await

--- bin/release.js
+++ bin/release.js
@@ -1,22 +1,12 @@
 #!/usr/bin/env node
 
 // Packages
-const asyncToGen = require('async-to-gen/register')
 const updateNotifier = require('update-notifier')
 const {red} = require('chalk')
 const nodeVersion = require('node-version')
 
 // Ours
 const pkg = require('../package')
-
-// Support for keywords "async" and "await"
-const pathSep = process.platform === 'win32' ? '\\\\' : '/'
-
-asyncToGen({
-  includes: new RegExp(`.*release?${pathSep}(lib|bin).*`),
-  excludes: null,
-  sourceMaps: false
-})
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
@@ -30,5 +20,4 @@ if (pkg.dist) {
   updateNotifier({pkg}).notify()
 }
 
-// Load package core with async/await support
 require('../lib')
