6e850026954282f356d340e614c3268370b1eae3
Leo Lamprecht <mindrun@icloud.com>
2017-04-13 23:19:37 +0200

    Removed all semicolons

--- bin/release.js
+++ bin/release.js
@@ -1,50 +1,50 @@
 #!/usr/bin/env node
 
 // Packages
-const args = require('args');
-const chalk = require('chalk');
-const semVer = require('semver');
-const inquirer = require('inquirer');
-const open = require('open');
-const { coroutine } = require('bluebird');
-const updateNotifier = require('update-notifier');
-const { red } = require('chalk');
-const nodeVersion = require('node-version');
+const args = require('args')
+const chalk = require('chalk')
+const semVer = require('semver')
+const inquirer = require('inquirer')
+const open = require('open')
+const { coroutine } = require('bluebird')
+const updateNotifier = require('update-notifier')
+const { red } = require('chalk')
+const nodeVersion = require('node-version')
 
 // Ours
-const groupChanges = require('../lib/group');
-const { branchSynced, getRepo } = require('../lib/repo');
-const getCommits = require('../lib/commits');
-const getChoices = require('../lib/choices');
-const getTags = require('../lib/tags');
-const definitions = require('../lib/definitions');
-const connect = require('../lib/connect');
-const createChangelog = require('../lib/changelog');
-const handleSpinner = require('../lib/spinner');
-const pkg = require('../package');
+const groupChanges = require('../lib/group')
+const { branchSynced, getRepo } = require('../lib/repo')
+const getCommits = require('../lib/commits')
+const getChoices = require('../lib/choices')
+const getTags = require('../lib/tags')
+const definitions = require('../lib/definitions')
+const connect = require('../lib/connect')
+const createChangelog = require('../lib/changelog')
+const handleSpinner = require('../lib/spinner')
+const pkg = require('../package')
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
   console.error(
     `${red('Error!')} Now requires at least version 6 of Node. Please upgrade!`
-  );
-  process.exit(1);
+  )
+  process.exit(1)
 }
 
 // Let user know if there's an update
 // This isn't important when deployed to Now
 if (pkg.dist) {
-  updateNotifier({ pkg }).notify();
+  updateNotifier({ pkg }).notify()
 }
 
 args
   .option('pre', 'Mark the release as prerelease')
-  .option('overwrite', 'If the release already exists, replace it');
+  .option('overwrite', 'If the release already exists, replace it')
 
-const flags = args.parse(process.argv);
+const flags = args.parse(process.argv)
 
-let githubConnection;
-let repoDetails;
+let githubConnection
+let repoDetails
 
 const changeTypes = [
   {
@@ -62,23 +62,23 @@ const changeTypes = [
     name: 'Patch',
     description: 'backwards-compatible bug fix'
   }
-];
+]
 
 const getReleaseURL = (release, edit = false) => {
   if (!release || !release.html_url) {
-    return false;
+    return false
   }
 
-  const htmlURL = release.html_url;
-  return edit ? htmlURL.replace('/tag/', '/edit/') : htmlURL;
-};
+  const htmlURL = release.html_url
+  return edit ? htmlURL.replace('/tag/', '/edit/') : htmlURL
+}
 
 const createRelease = (tag, changelog, exists) => {
-  const isPre = flags.pre ? 'pre' : '';
-  handleSpinner.create(`Uploading ${isPre}release`);
+  const isPre = flags.pre ? 'pre' : ''
+  handleSpinner.create(`Uploading ${isPre}release`)
 
-  const methodPrefix = exists ? 'edit' : 'create';
-  const method = methodPrefix + 'Release';
+  const methodPrefix = exists ? 'edit' : 'create'
+  const method = methodPrefix + 'Release'
 
   const body = {
     owner: repoDetails.user,
@@ -90,47 +90,47 @@ const createRelease = (tag, changelog, exists) => {
     body: changelog,
     draft: true,
     prerelease: flags.pre
-  };
+  }
 
   if (exists) {
-    body.id = exists;
+    body.id = exists
   }
 
   githubConnection.repos[method](body, (err, response) => {
     if (err || !response.data) {
-      console.log('\n');
-      handleSpinner.fail('Failed to upload release.');
+      console.log('\n')
+      handleSpinner.fail('Failed to upload release.')
     }
 
-    global.spinner.succeed();
-    const releaseURL = getReleaseURL(response.data, true);
+    global.spinner.succeed()
+    const releaseURL = getReleaseURL(response.data, true)
 
     if (releaseURL) {
-      open(releaseURL);
+      open(releaseURL)
     }
 
-    console.log(`\n${chalk.bold('Done!')} 🎉 Opening release in browser...`);
-  });
-};
+    console.log(`\n${chalk.bold('Done!')} 🎉 Opening release in browser...`)
+  })
+}
 
 const orderCommits = (commits, tags, exists) => {
-  const questions = [];
-  const predefined = {};
+  const questions = []
+  const predefined = {}
 
-  const choices = getChoices(changeTypes, tags);
+  const choices = getChoices(changeTypes, tags)
 
   // Show the latest changes first
-  commits.reverse();
+  commits.reverse()
 
   for (const commit of commits) {
-    const defTitle = definitions.type(commit.title, changeTypes);
-    const defDescription = definitions.type(commit.description, changeTypes);
+    const defTitle = definitions.type(commit.title, changeTypes)
+    const defDescription = definitions.type(commit.description, changeTypes)
 
-    const definition = defTitle || defDescription;
+    const definition = defTitle || defDescription
 
     if (definition) {
-      predefined[commit.hash] = definition;
-      continue;
+      predefined[commit.hash] = definition
+      continue
     }
 
     questions.push({
@@ -138,77 +138,77 @@ const orderCommits = (commits, tags, exists) => {
       message: commit.title,
       type: 'list',
       choices
-    });
+    })
   }
 
-  global.spinner.succeed();
+  global.spinner.succeed()
 
   // Prevents the spinner from getting succeeded
   // again once new spinner gets created
-  global.spinner = false;
+  global.spinner = false
 
   console.log(
     `${chalk.green('!')} Please enter the type of change for each commit:\n`
-  );
+  )
 
   inquirer.prompt(questions).then(
     coroutine(function*(types) {
       // Update the spinner status
-      console.log('');
-      handleSpinner.create('Generating the changelog');
+      console.log('')
+      handleSpinner.create('Generating the changelog')
 
-      const results = Object.assign({}, predefined, types);
-      const grouped = groupChanges(results, changeTypes);
-      const changelog = yield createChangelog(grouped, commits, changeTypes);
+      const results = Object.assign({}, predefined, types)
+      const grouped = groupChanges(results, changeTypes)
+      const changelog = yield createChangelog(grouped, commits, changeTypes)
 
       // Upload changelog to GitHub Releases
-      createRelease(tags[0], changelog, exists);
+      createRelease(tags[0], changelog, exists)
     })
-  );
-};
+  )
+}
 
 const collectChanges = (tags, exists = false) => {
-  handleSpinner.create('Loading commit history');
+  handleSpinner.create('Loading commit history')
 
   getCommits(tags).then(commits => {
     for (const commit of commits) {
       if (semVer.valid(commit.title)) {
-        const index = commits.indexOf(commit);
-        commits.splice(index, 1);
+        const index = commits.indexOf(commit)
+        commits.splice(index, 1)
       }
     }
 
     if (commits.length < 1) {
-      handleSpinner.fail('No changes happened since the last release.');
+      handleSpinner.fail('No changes happened since the last release.')
     }
 
-    orderCommits(commits, tags, exists);
-  });
-};
+    orderCommits(commits, tags, exists)
+  })
+}
 
 const checkReleaseStatus = coroutine(function*() {
-  let tags;
+  let tags
 
   try {
-    tags = yield getTags();
+    tags = yield getTags()
   } catch (err) {
-    handleSpinner.fail('Directory is not a Git repository.');
+    handleSpinner.fail('Directory is not a Git repository.')
   }
 
   if (tags.length < 1) {
-    handleSpinner.fail('No tags available for release.');
+    handleSpinner.fail('No tags available for release.')
   }
 
-  const synced = yield branchSynced();
+  const synced = yield branchSynced()
 
   if (!synced) {
-    handleSpinner.fail('Your branch needs to be up-to-date with origin.');
+    handleSpinner.fail('Your branch needs to be up-to-date with origin.')
   }
 
-  githubConnection = yield connect();
-  repoDetails = yield getRepo();
+  githubConnection = yield connect()
+  repoDetails = yield getRepo()
 
-  handleSpinner.create('Checking if release already exists');
+  handleSpinner.create('Checking if release already exists')
 
   githubConnection.repos.getReleases(
     {
@@ -217,50 +217,50 @@ const checkReleaseStatus = coroutine(function*() {
     },
     (err, response) => {
       if (err) {
-        handleSpinner.fail("Couldn't check if release exists.");
+        handleSpinner.fail("Couldn't check if release exists.")
       }
 
       if (!response.data || response.data.length < 1) {
-        collectChanges(tags);
-        return;
+        collectChanges(tags)
+        return
       }
 
-      let existingRelease = null;
+      let existingRelease = null
 
       for (const release of response.data) {
         if (release.tag_name === tags[0].tag) {
-          existingRelease = release;
-          break;
+          existingRelease = release
+          break
         }
       }
 
       if (!existingRelease) {
-        collectChanges(tags);
-        return;
+        collectChanges(tags)
+        return
       }
 
       if (flags.overwrite) {
-        global.spinner.text = 'Overwriting release, because it already exists';
-        collectChanges(tags, existingRelease.id);
+        global.spinner.text = 'Overwriting release, because it already exists'
+        collectChanges(tags, existingRelease.id)
 
-        return;
+        return
       }
 
-      global.spinner.succeed();
-      console.log('');
+      global.spinner.succeed()
+      console.log('')
 
-      const releaseURL = getReleaseURL(existingRelease);
+      const releaseURL = getReleaseURL(existingRelease)
 
       if (releaseURL) {
-        open(releaseURL);
+        open(releaseURL)
       }
 
-      const alreadyThere = 'Release already exists. Opening in browser...';
-      console.error(`${chalk.red('Error!')} ` + alreadyThere);
+      const alreadyThere = 'Release already exists. Opening in browser...'
+      console.error(`${chalk.red('Error!')} ` + alreadyThere)
 
-      process.exit(1);
+      process.exit(1)
     }
-  );
-});
+  )
+})
 
-checkReleaseStatus();
+checkReleaseStatus()
