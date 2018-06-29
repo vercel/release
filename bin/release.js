25d97e8b059930a2c9dfb825dfb81e6b37f5b67c
Tim Neutkens <tim@timneutkens.nl>
2018-06-29 09:25:01 +0200

    Shave off 16MB out of 17.3MB install size (#133)

--- bin/release.js
+++ bin/release.js
@@ -54,16 +54,19 @@ const changeTypes = [
 	{
 		handle: 'major',
 		name: 'Major Change',
+		pluralName: 'Major Changes',
 		description: 'incompatible API change'
 	},
 	{
 		handle: 'minor',
 		name: 'Minor Change',
+		pluralName: 'Minor Changes',
 		description: 'backwards-compatible functionality'
 	},
 	{
 		handle: 'patch',
 		name: 'Patch',
+		pluralName: 'Patches',
 		description: 'backwards-compatible bug fix'
 	}
 ];
